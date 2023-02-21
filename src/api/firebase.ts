3fcc08ca7e10371f9955e93008e9a0354f21aa85
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2023-02-22 10:55:51 +1100

    Add firebase to app

---
+++ src/api/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyDkigDLaDw_mj-0iaX-6BuSy5zuDRF0lPg',
+    authDomain: 'mhiebl-todo.firebaseapp.com',
+    projectId: 'mhiebl-todo',
+    storageBucket: 'mhiebl-todo.appspot.com',
+    messagingSenderId: '76012601756',
+    appId: '1:76012601756:web:55031a626fa6d1b9030859',
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
