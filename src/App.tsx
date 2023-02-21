921c9a787f107f6c6307b70d8718d9aac4d800b0
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2023-02-21 19:58:18 +1100

    Format all files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-const App = () => <BrowserRouter><Routes><Route path='/' element={<b>Hi</b>} /></Routes></BrowserRouter>
+const App = () => (
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<b>Hi</b>} />
+        </Routes>
+    </BrowserRouter>
+)
 
 export default App
