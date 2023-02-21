40bf379ae1a4f780b0fe74bd5c69710d72d57676
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2023-02-21 19:54:02 +1100

    APP-SETUP: init repo

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+const App = () => <BrowserRouter><Routes><Route path='/' element={<b>Hi</b>} /></Routes></BrowserRouter>
+
+export default App
